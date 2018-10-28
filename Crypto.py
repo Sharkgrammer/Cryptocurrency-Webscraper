b7c3cb930b755e52eaeffebd30bd79ae199e5488
Daniel Keane Kelly <gillskk@gmail.com>
2018-10-28 20:14:28 +0000

    Added argument option when calling file from command line

--- Crypto.py
+++ Crypto.py
@@ -1,4 +1,5 @@
 from lxml import html
+import sys
 import requests
 from colorama import init, Fore, Back, Style
 init(convert=True)
@@ -8,10 +9,17 @@ page = requests.get('https://coinmarketcap.com/')
 tree = html.fromstring(page.content)
 
 #Control Variables
-Lines = 4			#Controls how many lines the program shows (5 is generally the max)
-SpaceFix = 8		#The higher this is, the smaller the spaces are unless it surpasses the max length of the scraped date
-Colour = 0			#If 0, the percents will be in colour, if anything else, they'll be without colour
-StartVar = 0		#Adds spaces to the start of the data so that it isn't right beside the edge of the screen. If its 1 it'll calculate the space based off Lines and SpaceFix. If not 1, it wont add the space 
+Lines = 4           #Controls how many lines the program shows (5 is generally the max)
+SpaceFix = 8        #The higher this is, the smaller the spaces are unless it surpasses the max length of the scraped date
+Colour = 0          #If 0, the percents will be in colour, if anything else, they'll be without colour
+StartVar = 0        #Adds spaces to the start of the data so that it isn't right beside the edge of the screen. If its 1 it'll calculate the space based off Lines and SpaceFix. If not 1, it wont add the space 
+
+#If a number is passed when calling the program, have it set as the line variable
+if len(sys.argv) != 1:
+    try:
+        Lines = int(sys.argv[1])
+    except:
+        Lines = 4
 
 StartSpace = ""
 if StartVar == 1:
